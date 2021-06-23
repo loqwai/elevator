af83d8301f6f00b5e3faeb4942b000a81d016d3e
Roy van de Water <roy@circleci.com>
2021-06-23 16:42:10 -0700

    Gets to level 6

--- index.js
+++ index.js
@@ -1,45 +1,44 @@
 {
-    wrap: function() {
+  wrap: function() {
 
-    },
-    init: function(elevators, floors) {
-        let elevator
+  },
+  init: function(elevators, floors) {
+    let hellevators = new Set()
 
-        Elevator.prototype._userEntering = Elevator.prototype._userEntering ?? Elevator.prototype.userEntering
-        Elevator.prototype.userEntering = function (user) {
-            console.log('userEntering', this)
-            // this.MAXSPEED = Infinity
-            // this.ACCELERATION = Infinity
-            // this.DECELERATION = Infinity
-            elevator = this
-            return this._userEntering(user)
-        }
+    Movable.prototype.makeSureNotBusy = () => { }
+    Elevator.prototype._userEntering = Elevator.prototype._userEntering ?? Elevator.prototype.userEntering
+    Elevator.prototype.userEntering = function (user) {
+      console.log('userEntering', this)
+      // this.MAXSPEED = Infinity
+      // this.ACCELERATION = Infinity
+      // this.DECELERATION = Infinity
+      hellevators.add(this)
 
-        const teleport = (floorNum) => {
-            console.log('teleport')
-            if (!elevator) return
-
-            if (elevator.isBusy()) {
-                console.log('busy')
-                return requestAnimationFrame(() => teleport(floorNum))
-            }
-
-            console.log('elevator', elevator)
-            elevator.goToFloor(floorNum)
-            elevator.y = elevator.destinationY
-        }
-
-        floors.forEach(floor => {
-            floor.on('up_button_pressed', () => teleport(floor.floorNum()))
-            floor.on('down_button_pressed', () => teleport(floor.floorNum()))
-        })
-
-        elevators.forEach(e => {
-            e.on('floor_button_pressed', teleport)
-        })
-
-    },
-    update: function(dt, elevators, floors) {
-        // We normally don't need to do anything here
+      return this._userEntering(user)
     }
+
+    const teleport = (floorNum) => {
+      const handleHellevator = (hellevator) => {
+        if (hellevator.isBusy()) {
+          return requestAnimationFrame(() => handleHellevator(hellevator))
+        }
+        hellevator.goToFloor(floorNum)
+        hellevator.y = hellevator.destinationY
+      }
+
+      hellevators.forEach(handleHellevator)
+    }
+
+    floors.forEach(floor => {
+      floor.on('up_button_pressed', () => teleport(floor.floorNum()))
+      floor.on('down_button_pressed', () => teleport(floor.floorNum()))
+    })
+
+    elevators.forEach(e => {
+      e.on('floor_button_pressed', teleport)
+    })
+  },
+  update: function(dt, elevators, floors) {
+    // We normally don't need to do anything here
+  },
 }
